c8fd871316fc1268ab7550e10d882af9a078ab09
Joran Maille <joran.maille@gmail.com>
2017-04-14 22:50:42 +0200

    css and html first creation

--- fr/menuData.js
+++ fr/menuData.js
@@ -7,14 +7,17 @@ window.menuData = {
 	    id: 'language',
 	    children: [
 		{
+		    id: 'why',
 		    shortTitle: "Pourquoi?",
 		    title: "Pourquoi une nouvelle notation?",
 		},
 		{
+		    id: 'description',
 		    shortTitle: "SHATARO description",
 		    title: "Tout savoir sur le langage SHATARO"
 		},
 		{
+		    id: 'tools',
 		    shortTitle: "Outil de traduction",
 		    title: "Outil de traduction entre SHATARO et notation standard"
 		}
@@ -26,44 +29,54 @@ window.menuData = {
 	    id: 'beginner',
 	    children: [
 		{
+		    id: '1stCross',
 		    shortTitle: "La croix",
 		    title: "La première croix"
 		},
 		{
+		    id: '1stCrown',
 		    shortTitle: "Première couronne",
 		    title: "Faire la première couronne"
 		},
 		{
+		    id: '2ndCrown',
 		    shortTitle: "Deuxième couronne",
 		    title: "Compléter la deuxième couronne",
 		},
 		{
+		    id: '2ndCross_1',
 		    shortTitle: "Deuxième croix 1/2",
 		    title: "Faire la deuxième croix: étape 1"
 		},
 		{
+		    id: '2ndCross_2',
 		    shortTitle: "Deuxième croix 2/2",
 		    title: "Terminer la deuxième croix"
 		},
 		{
-		    shortTitle: "Face jaune"
+		    id: 'yellowFace',
+		    shortTitle: "Face jaune",
 		    title: "Compléter la face jaune"
 		},
 		{
+		    id: 'finalPhase',
 		    shortTitle: "Phase finale",
 		    title: "Comment terminer le cube"
 		}
+	    ]
 	},
 	{
 	    shortTitle: "Formules OLL et PLL",
 	    title: "Formules de résolution OLL et PLL en SHATARO",
-	    id: 'advanced'
+	    id: 'advanced',
 	    children: [
 		{
+		    id: 'oll',
 		    shortTitle: "OLL",
 		    title: "Formules SHATARO pour la résolution de la phase «OLL»"
 		},
 		{
+		    id: 'pll',
 		    shortTitle: "PLL",
 		    title: "Formules SHATARO pour la résolution de la phuse «PLL»"
 		}
@@ -71,23 +84,25 @@ window.menuData = {
 	},
 	{
 	    shortTitle: "Outils d'apprentissage OLL/PLL",
-	    title: "Outils d'apprentissage PLL et PLL (langage SHATARO et notation standard)"
-	    id: 'tools'
+	    title: "Outils d'apprentissage PLL et PLL (langage SHATARO et notation standard)",
+	    id: 'tools',
 	    children: [
 		{
+		    id: 'ollTrainer',
 		    shortTitle: "OLL Trainer",
 		    title: "OLL Trainer: s'entraîner à résoudre la phase OLL (cube physique nécessaire)"
 		},
 		{
+		    id: 'cubelessTrainer',
 		    shortTitle: "Cubeless Trainer",
 		    title: "Apprendre ou réviser les formules de résolution OLL et PLL."
 		}
 	    ]
 	},
-	{
-	    shortTitle: "WebGL cube",
-	    title: "Un cube en WebGL",
-	    id: 'cube'
-	}
+	// {
+	//     shortTitle: "WebGL cube",
+	//     title: "Un cube en WebGL",
+	//     id: 'cube'
+	// }
     ]
 };
--- src/web/frame.js
+++ src/web/frame.js
@@ -1,4 +1,9 @@
 (function() {
     "use strict";
-    
+    var topDiv = document.createElement("div");
+    topDiv.className = "topPane";
+    document.body.appendChild(topDiv);
+    var leftDiv = document.createElement("div");
+    leftDiv.className = "leftPane";
+    document.body.appendChild(leftDiv);
 })();
